fb3ae53db1ee60666dd79bebe85485a7ef07e66c
Drew <drewsup123c@gmail.com>
2019-10-07 23:30:07 -0600

    added spawn manager for enemies

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -15,12 +15,15 @@ public class Player : MonoBehaviour
     private float _fireRate = 0.5f;
     private float _nextFire = -1f;
     [SerializeField]
+    private SpawnManager _spawnManager;
     private int _lives = 3;
     // Start is called before the first frame update
     void Start()
     {
         //Starting Position -> position = new position(0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
+        // Updates variable spawn manager to the spawn manager component
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
     }
 
     // Update is called once per frame
@@ -64,6 +67,12 @@ public class Player : MonoBehaviour
     public void Damage(){
         _lives--;
         if(_lives < 1){
+            // Communicate with the spawn manager
+            if(_spawnManager != null){
+                _spawnManager.StopSpawnRoutine();
+            }else{
+                Debug.LogError("Spawn Manager is null!");
+            }
             Destroy(this.gameObject);
         }
     }
---
+++ Space Shooter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemy;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    private bool _B_keepSpawning = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Allows us to Yield events
+    IEnumerator SpawnRoutine(){
+        // while loop
+            // Instantiate enemy
+            // yield 5 seconds
+        while(_B_keepSpawning){
+            Vector3 position = new Vector3(Random.Range(-6, 6), 7, 0);
+            GameObject newEnemy = Instantiate(_enemy, position, Quaternion.identity);
+            newEnemy.transform.SetParent(_enemyContainer.transform);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    public void StopSpawnRoutine(){
+        _B_keepSpawning = false;
+    }
+}
